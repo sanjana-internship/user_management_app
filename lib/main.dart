aa3474be89e1a934434e4ad2ae07b7badccd9cb5
sanjana-internship <sanajanajaiswal018@gmail.com>
2025-04-25 00:56:50 -0700

    Initial commit of user management app

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
 // main.dart 
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'presentation/screens/home_screen.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'User Manager',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomeScreen(),
+    );
+  }
+}
